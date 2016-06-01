342c81535972e51ba5c37437ebcb76c17cc80ced
NightRa <ilan3580@gmail.com>
2016-06-01 21:52:06 +0300

    Initial commit, start MCTS

---
+++ src/mcts.cpp
@@ -0,0 +1,58 @@
+#include "mcts.h"
+#include "search.h"
+
+namespace Search {
+    bool is_terminal(Position &position);
+
+    double mctsSearch(Position &pos, MCTS_Node &root) {
+        // Updated by check_time()
+
+        int iteration = 0;
+        while (!Signals.stop) {
+            iteration++;
+
+            MCTS_Node *node = &root;
+
+            int rolloutResult;
+            double evalResult;
+
+            while (!is_terminal(pos) && !node->fully_opened()) {
+                MCTS_Edge *child = select_child_UCT(node);
+                node = &child->node;
+                pos.do_move(child->move, st, gives_check);
+            }
+
+            if (is_terminal(pos)) {
+
+                rolloutResult = get_result(pos, pos.side_to_move());
+                evalResult = rolloutResult;
+
+            } else { // at leaf / not fully opened.
+                MCTS_Edge *childEdge = node->open_child(pos);
+
+
+                node = &childEdge->node;
+                pos.do_move(childEdge->move, st, gives_check);
+
+                rolloutResult = rollout(pos);
+                evalResult = eval(pos, pos.side_to_move());
+            }
+
+            // Back propagation
+            while (node->incoming_edge != nullptr) {
+                node->incoming_edge->update_stats(rolloutResult, evalResult);
+                pos.undo_move(node->incoming_edge->move);
+                node = node->incoming_edge->parent;
+                // Update max stats in the parent
+                node->update_child_stats(rolloutResult, evalResult);
+                // Nega-max
+                rolloutResult = -rolloutResult;
+                evalResult = -evalResult;
+            }
+        }
+    }
+
+    bool is_terminal(Position &pos) {
+        return (pos.pieces() &~ pos.pieces(PAWN) &~ pos.pieces(KING)) != 0;
+    }
+}
---
+++ src/mcts.h
@@ -0,0 +1,60 @@
+#ifndef SRC_MCTS_H
+#define SRC_MCTS_H
+
+#include <vector>
+#include "types.h"
+
+typedef int NumVisits;
+typedef float EvalType;
+
+class MCTS_Node;
+
+struct MCTS_Edge {
+    MCTS_Node node;
+    Move move;
+    float prior;
+    EvalType evalSum;
+    NumVisits numEvals;
+    NumVisits rolloutsSum;
+    NumVisits numRollouts;
+    float overallEval;
+    MCTS_Node* parent;
+
+    void update_stats(int rolloutResult, double evalResult) {
+        rolloutsSum += rolloutResult;
+        numRollouts += 1;
+        evalSum += evalResult;
+        numEvals += 1;
+        overallEval = compute_overall_eval();
+    }
+
+private:
+    float compute_overall_eval() {
+        return (0.5f * (float(rolloutsSum) / numRollouts) + 0.5f * (evalSum / numEvals));
+    }
+};
+
+
+class MCTS_Node {
+public:
+    std::vector<MCTS_Edge> edges;
+    std::vector<Move> unopened_moves;
+    NumVisits maxVisits;
+    NumVisits totalVisits;
+    MCTS_Edge* incoming_edge;
+public:
+    MCTS_Node() : MCTS_Node(nullptr) {}
+    MCTS_Node(MCTS_Edge* parent): edges(0 /*Init with size 0*/), unopened_moves(0), maxVisits(0), totalVisits(0), incoming_edge(parent) {}
+
+    ~MCTS_Node() {
+        if(edges != nullptr) {
+            delete[] edges;
+        }
+    }
+
+    inline bool fully_opened() {
+        return unopened_moves.empty();
+    }
+};
+
+#endif //SRC_MCTS_H
